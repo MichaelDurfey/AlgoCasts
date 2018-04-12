a4fbfa2b69f15b50948a3cdada0508f9fc7dedf0
Michael Durfey <mdurfey@me.com>
2018-04-11 22:44:59 -0700

    init and work on BST

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,46 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(value) {
+    this.data = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    let queue = [this];
+    let inner = (node) => {
+      if (node.left) {
+        inner(node.left);
+      }
+      if (node.right) {
+        inner(node.right);
+      } 
+      if (node.left === null) {
+        node.left = new Node(data);
+      } else if (node.right === null) {
+        node.right = new Node(data);
+      }
+    }
+    inner(this);
+  }
+
+  contains(data) {
+    let queue = [this];
+    while (queue.length) {
+      let node = queue.shift();
+      if (node.value === data) {
+        return node;
+      } 
+      if (node.left) {
+        queue.push(node.left);
+      } 
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+  }
+}
 
 module.exports = Node;
