440f59efe18e604b7f724d4f7f1aa1a0093668f8
Michael Durfey <mdurfey@me.com>
2018-04-20 22:09:21 -0700

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,27 +7,25 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {
-  let newString = ''
-  for (let i = 0; i < str.length; i++) {
-    if (str[i - 1] === undefined || str[i - 1] === ' ') {
-      newString += str[i].toUpperCase();
-    } else {
-      newString += str[i];
-    }
-  }
-  return newString;
-}
+// function capitalize(str) {
+//   let newString = ''
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i - 1] === undefined || str[i - 1] === ' ') {
+//       newString += str[i].toUpperCase();
+//     } else {
+//       newString += str[i];
+//     }
+//   }
+//   return newString;
+// }
 
 capitalize('a short sentence')
 
 module.exports = capitalize;
 
-// function capitalize(str) {
-//   return str.split(' ').map(i =>  {
-//     let first = i[0]
-//     let theRest = i.split('').slice(1);
-//     return first.toUpperCase() + theRest.join('');
-//   }).join(' ')
+function capitalize(str) {
+  return str.split(' ').map(i =>  {
+    return i[0].toUpperCase() + i.slice(1);
+  }).join(' ')
 
-// }
+}
