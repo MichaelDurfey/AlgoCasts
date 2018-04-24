a3f0235a0a9a454ed0a272eda9067bbaa8f62a8f
Michael Durfey <mdurfey@me.com>
2018-04-23 21:50:32 -0700

    pyramid and vowels

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,46 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// From 0 to n (iterate through rows)
+  // create an empty string, 'level'
+  // from 0 to ??? (columns)
+    // If the column should have a #
+      //  Add a # to level
+    // else
+      // add a space to 'level
+    // console log 'stair'
+
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let hash = '';
+//     for (let col = 0; col < n * 2 - 1; col++) {
+//       if (midpoint + row >= col && midpoint - row <= col) {
+//         hash += '#'; 
+//       } else {
+//         hash += ' ';
+//       }
+//     }
+//     console.log(hash)
+//   }
+// }
+
+function pyramid(n, row = 0, hash = '') {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  if (row === n) {
+    return;
+  }
+  if (hash.length === (2 * n - 1)) {
+    console.log(hash);
+    return pyramid(n, row + 1)
+  }
+
+  if (midpoint + row >= hash.length && midpoint - row <= hash.length) {
+    hash += '#';
+  } else {
+    hash += ' ';
+  }
+  pyramid(n, row, hash);
+};
 
 module.exports = pyramid;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,26 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   const vowels = ['a', 'e', 'i', 'o', 'u'];
+//   let counter = 0;
+//   for (let char of str.toLowerCase()) {
+//     if (vowels.includes(char)) {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// }
+
+// function vowels(str) {
+//   return str.split('').reduce((acc, char) => {
+//     if (['a', 'e', 'i', 'o', 'u'].includes(char.toLowerCase())) acc++;
+//     return acc;
+//   }, 0);
+// }
+
+function vowels(str) {
+  return str.match(/[aeiou]/gi) ? str.match(/[aeiou]/gi).length : 0;
+}
 
 module.exports = vowels;
